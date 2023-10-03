c3e5e8d82942af8b8350bd066b5e85fb8d087923
Vegard Kyrkjedelen Hovland <vegardkh@stud.ntnu.no>
2023-10-03 17:46:37 +0200

    Made it possible to show dogs with multi-name names, and added basic styling.

--- src/components/breeds/breed-generator/BreedGenerator.tsx
+++ src/components/breeds/breed-generator/BreedGenerator.tsx
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from "react";
+import "./BreedGenerator.css"
+
+const makeInputToBreedUrl = (input: string): string => {
+    const words = input.trim().toLowerCase().split(" ")
+
+    let new_string = ""
+    for (let i = words.length - 1; i >= 0; i--) {
+        new_string += words[i]
+        if (i !== 0) {
+            new_string += "/"
+        }
+    }
+    return new_string
+}
 
 const DogGenerator:React.FC  = () => {
     const [breedInputValue, setBreedInputValue] = useState<string>("");
+    const [dogBreedUrl, setDogBreedUrl] = useState<string>("");
     const [dogUrl, setDogUrl] = useState<string>("");
     const [noBreedError, setNoBreedError] = useState<string>("");
 
     const updateDog = async () => {
-        const response = await fetch(`https://dog.ceo/api/breed/${breedInputValue}/images/random`)
+        const response = await fetch(`https://dog.ceo/api/breed/${dogBreedUrl}/images/random`)
         const json = await response.json();
 
         if (json.code === 404) {
@@ -18,10 +33,13 @@ const DogGenerator:React.FC  = () => {
         }
     }
     const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
-        console.log((event.target as HTMLInputElement).value)
-        setBreedInputValue((event.target as HTMLInputElement).value.trim().toLowerCase());
+        const value = (event.target as HTMLInputElement).value
+        console.log(value)
+        setBreedInputValue(value.trim().toLowerCase());
+        setDogBreedUrl(makeInputToBreedUrl(value))
       };
     const updateAll = () => {
+        console.log("Click!")
         if (breedInputValue.trim() !== "") {
             updateDog();
         }
@@ -32,7 +50,7 @@ const DogGenerator:React.FC  = () => {
     return <>
         <div className="breed-generator">
             <input type="text" onChange={handleInputChange}/>
-            <button onClick={updateAll}>Klikk</button>
+            <button onClick={updateAll}>Trykk her for å se mer dog.</button>
             { dogUrl !== "" &&
                 <img src={dogUrl} />
             }
