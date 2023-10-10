85fe97201f5f92af0fc9444a6d68207af40e0f47
Vegard Kyrkjedelen Hovland <vegardkh@stud.ntnu.no>
2023-10-10 18:25:17 +0200

    Changed css-style to be more responsive.

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./About.css"
 
 const About:React.FC = () => {
     return <>
-        <main style={{color: "black"}}>
+        <main className="main"  style={{color: "black"}}>
             <h1>About us</h1>
             <a href="https://github.com/kyrkjedelen/dogpage/tree/main">Github</a>
             <p>Dette er et prosjekt lagd i forbindelse med <a target="_blank" href="https://vektorprogrammet.no">vektorprogrammet</a>.</p>
