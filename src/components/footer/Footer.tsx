a6290ddfa64fc277b12fb4d9c19655db42c53fcb
Kaja Prestnes Lind <kajaprestneslind@dhcp-10-22-20-131.wlan.ntnu.no>
2023-10-24 17:47:50 +0200

    fix dark mode

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,20 +2,22 @@
 import './Footer.css'
 
 function Footer() {
+    
     return(
         <footer id = 'footer'>
-            <div id="SoMe">
+            <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"></link>
+            <div id="soMe">
                 <a id="reddit" href='https://old.reddit.com/r/Eyebleach/' target="_blank" rel="noopener noreferrer">
-                    <img src='https://cdn.icon-icons.com/icons2/2428/PNG/512/reddit_black_logo_icon_147088.png' alt="Reddit Icon" />
+                    <i className='bx bxl-reddit bx-lg'></i>
                 </a>
                 <a id="linkedin" href='https://justthetip.fm/wp-content/uploads/season01/episode13/shownote-devitoass.jpg' target="_blank" rel="noopener noreferrer">
-                    <img src='https://www.edigitalagency.com.au/wp-content/uploads/new-linkedin-logo-white-black-png.png' alt="LinkedIn Icon" />
+                    <i className='bx bxl-linkedin-square bx-lg'></i>
                 </a>
                 <a id="github" href='https://github.com/orgs/vektorprogrammet/teams/vektorweb' target="_blank" rel="noopener noreferrer">
-                    <img src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="Github Icon" />
+                    <i className='bx bxl-github bx-lg'></i>
                 </a>
                 <a id="instagram" href='https://www.instagram.com/doge.jpg_/' target="_blank" rel="noopener noreferrer">
-                    <img src='https://icons.veryicon.com/png/o/miscellaneous/offerino-icons/instagram-53.png' alt="Instagram Icon" />
+                    <i className='bx bxl-instagram bx-lg'></i>
                 </a>
             </div>
 
