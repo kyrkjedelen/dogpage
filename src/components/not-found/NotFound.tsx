a44dda681498ca52249d31a87e7658027ed6a260
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2023-11-02 17:10:30 +0100

    Fixed not found and footer styling.

--- src/components/not-found/NotFound.tsx
+++ src/components/not-found/NotFound.tsx
@@ -1,8 +1,9 @@
+import styles from "./NotFound.module.css";
 
 const NotFound:React.FC = () => {
     return (
         <>
-            <main>
+            <main className={styles.main}>
                 <h1>404 not found</h1>
                 <p>The page you are searching for was not found.</p>
             </main>
