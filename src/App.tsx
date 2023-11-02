36a31dc95262d675a922b9a65365bb1bba5f3576
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2023-11-02 17:30:47 +0100

    Fixed not found page.

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ const App:React.FC = () => {
           <Route path="/about" element={<About />} />
           <Route path="/favourites" element={<Favourites />} />
           <Route path="/breeds" element={<Breeds />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </BrowserRouter>
